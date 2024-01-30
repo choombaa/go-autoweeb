3f00de9918926fcf64a9c9a0322fecd5c931dfd9
choomba <beginanon@gmail.com>
2024-01-29 22:19:55 -0800

    Replaced boilerplate tabs with spaces

--- main.go
+++ main.go
@@ -1,39 +1,39 @@
 package main
 
 import (
-	"net/http"
+  "net/http"
   "go-autoweeb/routes"
-	"github.com/gin-gonic/gin"
+  "github.com/gin-gonic/gin"
   "github.com/gin-contrib/cors"
 )
 
 var db = make(map[string]string)
 
 func setupRouter() *gin.Engine {
-	// Disable Console Color
-	// gin.DisableConsoleColor()
-	router := gin.Default()
+  // Disable Console Color
+  // gin.DisableConsoleColor()
+  router := gin.Default()
   // TODO Default CORS doesn't play nice with Authorization header
   router.Use(cors.Default())
 
   routes.LoadV1Routes(router);
 
-	// Get user value
-	router.GET("/user/:name", func(c *gin.Context) {
-		user := c.Params.ByName("name")
-		value, ok := db[user]
-		if ok {
-			c.JSON(http.StatusOK, gin.H{"user": user, "value": value})
-		} else {
-			c.JSON(http.StatusOK, gin.H{"user": user, "status": "no value"})
-		}
-	})
+  // Get user value
+  router.GET("/user/:name", func(c *gin.Context) {
+    user := c.Params.ByName("name")
+    value, ok := db[user]
+    if ok {
+      c.JSON(http.StatusOK, gin.H{"user": user, "value": value})
+    } else {
+      c.JSON(http.StatusOK, gin.H{"user": user, "status": "no value"})
+    }
+  })
 
-	return router
+  return router
 }
 
 func main() {
-	router := setupRouter()
-	// Listen and Server in 0.0.0.0:8080
-	router.Run(":8080")
+  router := setupRouter()
+  // Listen and Server in 0.0.0.0:8080
+  router.Run(":8080")
 }
--- routes/v1.go
+++ routes/v1.go
@@ -1,7 +1,7 @@
 package routes
 
 import (
-	"github.com/gin-gonic/gin"
+  "github.com/gin-gonic/gin"
   "go-autoweeb/controllers"
 )
 
