c49fc37d0dd8f91315849f5cf27c9fa3e602f424
choomba <beginanon@gmail.com>
2024-01-30 11:12:08 -0800

    Removed boilerplate

--- main.go
+++ main.go
@@ -7,8 +7,6 @@ import (
   "github.com/gin-contrib/cors"
 )
 
-var db = make(map[string]string)
-
 func setupRouter() *gin.Engine {
   // Disable Console Color
   // gin.DisableConsoleColor()
@@ -17,17 +15,6 @@ func setupRouter() *gin.Engine {
   router.Use(cors.Default())
 
   routes.LoadV1Routes(router);
-
-  // Get user value
-  router.GET("/user/:name", func(c *gin.Context) {
-    user := c.Params.ByName("name")
-    value, ok := db[user]
-    if ok {
-      c.JSON(http.StatusOK, gin.H{"user": user, "value": value})
-    } else {
-      c.JSON(http.StatusOK, gin.H{"user": user, "status": "no value"})
-    }
-  })
 
   return router
 }
